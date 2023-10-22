7b92b4b636d4f051be8ef559f094ec522910af05
Parakh Jain <pjmactophone@gmail.com>
2023-10-22 23:22:28 +0530

    Tailwind and Props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Card username = "Parakh Jain" btnText = "Click Me"/>
+      <Card username = "Paras J"/>
+    </>
+  )
+}
+
+export default App
